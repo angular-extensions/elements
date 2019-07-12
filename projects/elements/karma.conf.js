1044d9c048ca27fe6cafcfedf28cf50c8facaf12
tomastrajan <tomas.trajan@gmail.com>
2019-07-12 14:53:25 +0200

    fix(workspace): code coverage folder

--- projects/elements/karma.conf.js
+++ projects/elements/karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage/elements-demo'),
+      dir: require('path').join(__dirname, '../../coverage/elements'),
       reports: ['html', 'lcovonly', 'text-summary', 'json'],
       fixWebpackSourcePaths: true
     },
