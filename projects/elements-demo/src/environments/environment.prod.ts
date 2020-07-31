80ea4211cf47711f9cdc5a271cb70d94da8e4fd3
tomastrajan <tomas.trajan@gmail.com>
2020-07-31 10:51:31 +0200

    chore(release): 10.0.2

--- projects/elements-demo/src/environments/environment.prod.ts
+++ projects/elements-demo/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   // version:start
-  version: '10.0.1',
+  version: '10.0.2',
   // version:end
   production: true,
 };
--- projects/elements-demo/src/environments/environment.ts
+++ projects/elements-demo/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   // version:start
-  version: '10.0.1',
+  version: '10.0.2',
   // version:end
   production: false,
 };
