718fedb37f58de554b170f5736b501a490f5695a
tomastrajan <tomas.trajan@gmail.com>
2023-03-31 10:52:19 +0200

    chore(demo): add contributor, fix code block overflow

--- sync-contributors.js
+++ sync-contributors.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const SOURCE = '.all-contributorsrc';
 const TARGET =
-  './projects/elements-demo/src/app/features/contribute/contribute/contribute.component.html';
+  './projects/elements-demo/src/app/features/contribute/contribute.component.html';
 const TOKEN_START = '<!-- ALL-CONTRIBUTORS-LIST:START -->';
 const TOKEN_END = '<!-- ALL-CONTRIBUTORS-LIST:END -->';
 const PATTERN = new RegExp(`${TOKEN_START}[\\s\\S]*${TOKEN_END}`, 'gim');
@@ -12,7 +12,7 @@ const content = fs.readFileSync(TARGET, 'utf8');
 
 const data = contributors
   .map(
-    c => `
+    (c) => `
   <a class="contributor" href="${c.profile}" target="_blank">
     <img src="${c.avatar_url}">
     <span>${c.name}</span>
