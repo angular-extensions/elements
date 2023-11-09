4d69ec533f8f0967fafb347273a70963cb69aa5f
tomastrajan <tomas.trajan@gmail.com>
2023-11-09 17:18:01 +0100

    chore(release): 17.0.0

--- projects/elements-demo/src/environments/environment.prod.ts
+++ projects/elements-demo/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   // version:start
-  version: '16.0.0',
+  version: '17.0.0',
   // version:end
   production: true,
 };
--- projects/elements-demo/src/environments/environment.ts
+++ projects/elements-demo/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   // version:start
-  version: '16.0.0',
+  version: '17.0.0',
   // version:end
   production: false,
 };
